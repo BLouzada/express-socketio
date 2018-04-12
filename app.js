1a10c8082e75ed36fd60548b2242892bbae66af2
Bruno Louzada <blouzada@live.com>
2018-04-11 21:57:39 -0300

    show me the code 1 conexao e envio de eventos

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+app.sockets = {}
 app.get('/', function (req, res) {
   res.sendfile('public/index.html');
 });
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  const socketId = socket.id
+  app.sockets[socketId] = socket.id
+  io.to(socketId).emit('socket.id.received', {id: socketId})
+  io.sockets.emit('socket.list', {sockets: app.sockets});
+  socket.on('disconnect', (reason) => {
+    const socketId = socket.id
+    app.sockets[socketId] = undefined
+    io.sockets.emit('socket.list', {sockets: app.sockets});
   });
 });
 module.exports = server
