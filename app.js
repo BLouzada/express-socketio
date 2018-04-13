7855a3fc07f030d4e12b8c6604896a0dea43ee03
Bruno Louzada <blouzada@live.com>
2018-04-12 21:37:35 -0300

    tratando e emitindo eventos

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ app.sockets = {}
 app.get('/', function (req, res) {
   res.sendfile('public/index.html');
 });
+app.get('/emit-event', function (req, res) {
+  io.sockets.emit('new.event', {newEvent: {data: 'exemplo', date: new Date().toISOString()}});
+  res.end()
+});
 io.on('connection', function (socket) {
   const socketId = socket.id
+  console.log(`Socket conectado ${socketId}`)
   app.sockets[socketId] = socket.id
   io.to(socketId).emit('socket.id.received', {id: socketId})
   io.sockets.emit('socket.list', {sockets: app.sockets});
   socket.on('disconnect', (reason) => {
     const socketId = socket.id
+    console.log(`Socket desconectado ${socketId}`)
     app.sockets[socketId] = undefined
     io.sockets.emit('socket.list', {sockets: app.sockets});
   });
